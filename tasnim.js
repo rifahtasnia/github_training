c398e55bb358c2d7ad9a9ac0d052a9b11a4b7961
ehsanrishat <erishat162079@bscse.uiu.ac.bd>
2022-02-12 00:29:34 +0600

    Added file tasnim.js

---
+++ tasnim.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello this is Tasnim Jarin Afra doing internship at IQVIA and this is part of the assignment from the github session";
+}
+
