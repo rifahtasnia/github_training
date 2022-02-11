484bc8e4945f22eecfc66c1d11392ea8a53565c0
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2022-02-11 22:51:03 +0600

    added js file and updated myfile

---
+++ rifah.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "I am Khandaker Rifah Tasnia. Currently, I am in my 8th Semester pursuing B.Sc in Software Engineering at Islamic University of Technology.";
+}
+
